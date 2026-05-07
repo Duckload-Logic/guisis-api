ee0f26c0eea8612bf562fe982fd53d5977de12d9
olazo-johnalbert <jaolazo4@gmail.com>
2026-05-07 10:43:36 +0800

    feat: add DTOs for OGOS student integration requests and responses

--- internal/features/students/integrations/dto.go
+++ internal/features/students/integrations/dto.go
@@ -44,7 +44,7 @@ type OGOSStudentPersonalInfoDTO struct {
 	Gender       students.Gender `json:"gender"`
 	DateOfBirth  string          `json:"dateOfBirth"`
 	PlaceOfBirth string          `json:"placeOfBirth"`
-	HeightM      float32         `json:"heightM"`
+	HeightM      float32         `json:"heightFt"`
 	WeightKg     float32         `json:"weightKg"`
 }
 
