33df41df480feb8839ae3f0f95944dfbd778fbcf
olazo-johnalbert <jaolazo4@gmail.com>
2026-06-13 20:39:37 +0800

    style(m2m): format client model tag

--- internal/features/m2mclients/models.go
+++ internal/features/m2mclients/models.go
@@ -5,16 +5,15 @@ import (
 )
 
 type M2MClient struct {
-	ID                int       `db:"id"                 json:"id"`
-	UserID            string    `db:"user_id"            json:"userId"`
-	ClientName        string    `db:"client_name"        json:"clientName"`
-	ClientID          string    `db:"client_id"          json:"clientId"`
-	ClientSecret      string    `db:"client_secret_hash" json:"-"`
-	ClientDescription string    `db:"client_description" json:"clientDescription"`
-	IsActive          bool      `db:"is_active"          json:"isActive"`
-	IsVerified        bool      `db:"is_verified"        json:"isVerified"`
-	HasPersonalInfoAccess bool `db:"has_personal_info_access"
-		json:"hasPersonalInfoAccess"`
-	CreatedAt         time.Time `db:"created_at"         json:"createdAt"`
-	UpdatedAt         time.Time `db:"updated_at"         json:"updatedAt"`
+	ID                    int       `db:"id"                 json:"id"`
+	UserID                string    `db:"user_id"            json:"userId"`
+	ClientName            string    `db:"client_name"        json:"clientName"`
+	ClientID              string    `db:"client_id"          json:"clientId"`
+	ClientSecret          string    `db:"client_secret_hash" json:"-"`
+	ClientDescription     string    `db:"client_description" json:"clientDescription"`
+	IsActive              bool      `db:"is_active"          json:"isActive"`
+	IsVerified            bool      `db:"is_verified"        json:"isVerified"`
+	HasPersonalInfoAccess bool      `db:"has_personal_info_access" json:"hasPersonalInfoAccess"`
+	CreatedAt             time.Time `db:"created_at"         json:"createdAt"`
+	UpdatedAt             time.Time `db:"updated_at"         json:"updatedAt"`
 }
